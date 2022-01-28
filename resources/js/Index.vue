ca794c38dc8b12c835eec30b174b5b8c8a20d078
Ronel <ronel.andaya@gmail.com>
2022-01-29 07:27:02 +0800

    Styling the navigation bar

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <router-link :to="{name:'home'}">Home</router-link>
-        <router-link :to="{name:'second'}">Second</router-link>
-        <router-view></router-view>
+        <div class="navbar bg-white border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" :to="{name:'home'}">Wei-BNB</router-link>
+            <router-link class="btn nav-button" :to="{name:'second'}">Second</router-link>
+        </div>
+        <div class="container mt-4 mb-4 pr-4 pl-4">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
