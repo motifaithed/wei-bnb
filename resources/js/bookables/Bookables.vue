759520489037c7a792b8eeb5913a5d18bea7a9c2
Ronel <ronel.andaya@gmail.com>
2022-01-30 10:05:32 +0800

    Component props and prop types

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <bookable-list-item></bookable-list-item>
+        <bookable-list-item item-title="Deca Villa 1" item-content="this is deca villa 1" :item-price="1000"></bookable-list-item>
+        <bookable-list-item item-title="Deca Villa 2" item-content="this is deca villa 2" :item-price="1500"></bookable-list-item>
     </div>
 </template>
 <script>
