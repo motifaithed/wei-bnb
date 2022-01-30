35c0423c2aa0de3d15414801f8f6e58d5e0bd1e1
Ronel <ronel.andaya@gmail.com>
2022-01-31 02:58:38 +0800

    Request input validation(validating data clients send to our server)

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -16,6 +16,9 @@ class BookableAvailabilityController extends Controller
     public function __invoke(Request $request)
     {
         //
-        dd('it works');
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
     }
 }
