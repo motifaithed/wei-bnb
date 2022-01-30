96c5b68edd6815ddc09b52c8b04ee85bd892eaf1
Ronel <ronel.andaya@gmail.com>
2022-01-30 17:35:07 +0800

    Using Model::find and mandatory/optional route

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('bookables', function(Request $request){
-    
+
     return Bookable::all();
+});
+
+Route::get('bookables/{id}', function (Request $request, $id){
+
+    return Bookable::find($id);
 });
