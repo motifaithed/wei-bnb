d37db73e33790588ca10db6bf6d1ff90f95403a4
Ronel <ronel.andaya@gmail.com>
2022-02-10 01:24:40 +0800

    Reviews controller using latest query scope

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\BookablesController;
 use App\Http\Controllers\Api\BookableAvailabilityController;
+use App\Http\Controllers\Api\BookableReviewController;
 use App\Models\Bookable;
 
 use Illuminate\Http\Request;
@@ -27,3 +28,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('bookables', BookablesController::class)->only('index','show');
 Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)->name('bookables.availability.show');
+Route::get('bookables/{bookable}/review', BookableReviewController::class)->name('bookables.reviews.index');
