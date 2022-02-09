9f0e8aa1d9cf5a4b0e9ac189c369230cca348916
Ronel <ronel.andaya@gmail.com>
2022-02-10 01:05:43 +0800

    Review model factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(BookablesTableSeeder::class);
         $this->call(BookingsTableSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
     }
 }
