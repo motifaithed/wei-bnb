e64894fdfed3c945c9a9bfc74eceb537c59953ae
Ronel <ronel.andaya@gmail.com>
2022-02-16 18:03:46 +0800

    Booking review key and Eloquent model events

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Booking extends Model
 {
@@ -24,5 +25,14 @@ class Booking extends Model
         
         return $query->where('to', '>=', $from)
                      ->where('from', '<=', $to);
+    }
+    protected static function boot(){
+
+        //required
+        parent::boot();
+
+        static::creating(function ($booking){
+            $booking->review_key = Str::uuid();
+        });
     }
 }
