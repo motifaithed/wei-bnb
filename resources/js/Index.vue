0a6caeb397fdf86089638cd64e5487fba1a3b8c9
Ronel <ronel.andaya@gmail.com>
2022-03-02 15:42:08 +0800

    Binding to Vuex state

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -8,3 +8,19 @@
         </div>
     </div>
 </template>
+<script>
+import {mapState} from 'vuex';
+export default {
+    data(){
+        return {
+            lastSearch: this.$store.state.lastSearch
+        }
+    },
+    computed:{
+        ...mapState({
+            lastSearchComputed: "lastSearch"
+        })
+    }
+
+}
+</script>
